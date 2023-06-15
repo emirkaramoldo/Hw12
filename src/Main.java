cc6b8b3ec799a395dbb8ae0083fe8be97b7085bf
emirkaramoldoev <emirkaramoldoev1@gmail.com>
2023-06-15 23:07:19 +0600

    Создан цикл

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,18 @@ public class Main {
         ArrayList<String> musicalInstruments = new ArrayList<>();
         ArrayList<String> musicalInstruments2 = new ArrayList<>();
         ArrayList<String> musicalInstruments3 = new ArrayList<>();
+        while (i < 10){
+            System.out.println("Введите значение");
+            String name = input.next();
+            i++;
+            System.out.println(name);
+            System.out.println(i);
+            if(i <= 5){
+                musicalInstruments.add(name);
+            }
+            else {
+                musicalInstruments2.add(name);
+            }
+        }
     }
 }
